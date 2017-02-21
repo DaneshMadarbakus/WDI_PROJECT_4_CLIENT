e499a83b713d9c941f72438e98f9be249e51422d
DaneshMadarbakus <daneshplt@hotmail.co.uk>
2017-02-21 21:28:57 +0000

    created request factory

---
+++ src/js/factories/request.factory.js
@@ -0,0 +1,12 @@
+angular
+  .module('rent-a-suit')
+  .factory('Request', requestFactory);
+
+requestFactory.$inject = ['API', '$resource'];
+function requestFactory(API, $resource){
+  return $resource(`${API}/requests/:id`, { id: '@_id'},
+    {
+      'save': {method: 'POST'}
+    }
+  );
+}

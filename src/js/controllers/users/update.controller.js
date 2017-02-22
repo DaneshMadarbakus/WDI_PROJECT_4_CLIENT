35e7b45f4affcf04712aef4b31167c089a3780f8
DaneshMadarbakus <daneshplt@hotmail.co.uk>
2017-02-22 16:22:53 +0000

    fixed user update controller

---
+++ src/js/controllers/users/update.controller.js
@@ -0,0 +1,24 @@
+angular
+  .module('rent-a-suit')
+  .controller('UsersUpdateCtrl', UsersUpdateCtrl);
+
+UsersUpdateCtrl.$inject = ['User', '$state', '$stateParams'];
+function UsersUpdateCtrl(User, $state, $stateParams){
+  const vm = this;
+  User
+    .get($stateParams)
+    .$promise
+    .then(response => {
+      console.log(response);
+      vm.user = response;
+    });
+
+  vm.edit = () => {
+    User
+      .update($stateParams, vm.user)
+      .$promise
+      .then(() => {
+        $state.go('usersShow({ id: $stateParams })');
+      });
+  };
+}
